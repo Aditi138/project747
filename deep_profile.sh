b27a000f00d42d2794e57850fb1ea60a3204a751
Michiel29 <michiel.s.dejong@gmail.com>
2018-04-20 21:20:44 -0400

    deep profile, temporarily change data loader

--- deep_profile.sh
+++ deep_profile.sh
@@ -2,10 +2,11 @@ nvprof --profile-from-start off -o profiles/context_deep.prof -- python context.
 --train_path ../pickle/train_summaries.pickle  \
 --valid_path ../pickle/valid_summaries.pickle \
 --test_path ../pickle/test_summaries.pickle \
-# --train_path ../pickle/train_summaries.pickle  \
-# --valid_path ../pickle/valid_summaries.pickle \
-# --test_path ../pickle/test_summaries.pickle \
 --hidden_size 128 \
 --embed_size 128 \
 --eval_interval 10 \
 --num_epochs 1
+
+# --train_path ../pickle/train_summaries.pickle  \
+# --valid_path ../pickle/valid_summaries.pickle \
+# --test_path ../pickle/test_summaries.pickle \
