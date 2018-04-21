fixing softmax

context.py
@@ -90,8 +90,6 @@ def train_epochs(model, vocab):
 	patience = 30
 
 	valid_batches = create_batches(valid_documents, args.batch_length, args.job_size, vocab)[:200]
-	train_batches = create_batches(train_documents, args.batch_length, args.job_size, vocab)
-	# train_batch_for_validation = get_random_batch_from_training(train_batches, args.batch_length)
 	test_batches = create_batches(test_documents,args.batch_length,args.job_size, vocab)
 
 	for epoch in range(args.num_epochs):
@@ -138,16 +136,21 @@ def train_epochs(model, vocab):
 				# query tokens
 				batch_query = variable(torch.LongTensor(query))
 				batch_query_length = np.array([batch['qlengths'][index]])
+				batch_question_mask = np.array([batch['q_mask'][index]])
+
 				# batch_query_ner = variable(torch.LongTensor(batch['q_ner'][index]))
 				# batch_query_pos = variable(torch.LongTensor(batch['q_pos'][index]))
 
 				# Sort the candidates by length (only required if using an RNN)
 				batch_candidate_lengths = np.array(batch_candidates["anslengths"][index])
+				batch_candidate_mask = np.array(batch_candidates['mask'][index])
 				candidate_sort = np.argsort(batch_candidate_lengths)[::-1].copy()
+
 				batch_candidates_sorted = variable(
 					torch.LongTensor(batch_candidates["answers"][index][candidate_sort, ...]))
 				batch_candidate_lengths_sorted = batch_candidate_lengths[candidate_sort]
 				batch_candidate_unsort = variable(torch.LongTensor(np.argsort(candidate_sort)))
+				batch_candidate_masks_sorted = variable(torch.LongTensor(batch_candidate_mask[candidate_sort]))
 
 				# batch_candidate_ner_sorted = variable(
 				# 	torch.LongTensor(batch_candidates['ner'][index][candidate_sort, ...]))
@@ -161,15 +164,17 @@ def train_epochs(model, vocab):
 				# context tokens
 				batch_context = variable(torch.LongTensor(batch['contexts'][index]))
 				batch_context_length = np.array([batch['clengths'][index]])
+				batch_context_mask = np.array([batch['context_mask'][index]])
 
 				gold_index = variable(torch.LongTensor([batch_answer_indices[index]]))
 				negative_indices = [idx for idx in range(batch_len)]
 				negative_indices.pop(batch_answer_indices[index])
 				negative_indices = variable(torch.LongTensor(negative_indices))
 
-				loss, second_best = model(batch_query, batch_query_length,
-									batch_context, batch_context_length,
-									batch_candidates_sorted, batch_candidate_lengths_sorted, batch_candidate_unsort,
+				loss, second_best = model(batch_query, batch_query_length,batch_question_mask,
+									batch_context, batch_context_length, batch_context_mask,
+									batch_candidates_sorted, batch_candidate_lengths_sorted, batch_candidate_masks_sorted,
+										  batch_candidate_unsort,
 									gold_index, negative_indices, batch_metrics
 									)
 

models/bidaf.py
@@ -9,34 +9,88 @@ class BiDAF(nn.Module):
 		self.input_size = input_size
 		self.similarity_layer = nn.Linear(3*input_size, 1)
 
+	def masked_softmax(self, vector, mask):
+		"""
+        ``torch.nn.functional.softmax(vector)`` does not work if some elements of ``vector`` should be
+        masked.  This performs a softmax on just the non-masked portions of ``vector``.  Passing
+        ``None`` in for the mask is also acceptable; you'll just get a regular softmax.
+        We assume that both ``vector`` and ``mask`` (if given) have shape ``(batch_size, vector_dim)``.
+        In the case that the input vector is completely masked, this function returns an array
+        of ``0.0``. This behavior may cause ``NaN`` if this is used as the last layer of a model
+        that uses categorical cross-entropy loss.
+        """
+		if mask is None:
+			result = torch.nn.functional.softmax(vector, dim=-1)
+		else:
+			# To limit numerical errors from large vector elements outside the mask, we zero these out.
+			result = torch.nn.functional.softmax(vector * mask, dim=-1)
+			result = result * mask
+			result = result / (result.sum(dim=1, keepdim=True) + 1e-13)
+		return result
+
+	def replace_masked_values(self,tensor, mask, replace_with):
+		"""
+		Replaces all masked values in ``tensor`` with ``replace_with``.  ``mask`` must be broadcastable
+		to the same shape as ``tensor``. We require that ``tensor.dim() == mask.dim()``, as otherwise we
+		won't know which dimensions of the mask to unsqueeze.
+		"""
+		# We'll build a tensor of the same shape as `tensor`, zero out masked values, then add back in
+		# the `replace_with` value.
+		if tensor.dim() != mask.dim():
+			raise Exception("tensor.dim() (%d) != mask.dim() (%d)" % (tensor.dim(), mask.dim()))
+		one_minus_mask = 1.0 - mask
+		values_to_add = replace_with * one_minus_mask
+		return tensor * mask + values_to_add
+
 	## TODO: Add capability of sentence mask (scoring) for sentence selection model
-	def forward(self, U, H):
-		T = H.size(1)
-		J = U.size(1)
+	def forward(self, U, H, U_mask, H_mask): #H:context U: query
+		T = H.size(1)   #Context Length
+		J = U.size(1)  #Quesiton Length
+
+		batch_size = U.size(0)
+
 
 		## compute S from H and U
 		## shape of ctx_C: (N, T, 2d) and ctx_Q : # (N, J, 2d)
 		## make both matrices of shape (N, T, J, 2d) to compute S
-		expanded_H = H.unsqueeze(2).expand((-1,-1, J,-1)) # (N, T, J, 2d)
-		expanded_U = U.unsqueeze(1).expand(-1, T, -1,-1) # (N, T, J, 2d)
-		HU = torch.mul(expanded_H, expanded_U) # (N, T, J, 4d)
-		cat_data = torch.cat((expanded_H, expanded_U, HU), 3) # (N, T, J, 6d)
-		S = self.similarity_layer(cat_data).view(-1, T, J) # (N, T, J, 1) => (N, T, J)
+
+		expanded_U = U.unsqueeze(2).expand((U.size(0),U.size(1),H.size(1),U.size(2))) # (N, T, J, 2d)
+		expanded_H = H.unsqueeze(1).expand((H.size(0), U.size(1), H.size(1), H.size(2)))  # (N, T, J, 2d)
+
+		S = (expanded_U * expanded_H).sum(dim=-1)
+
+		#HU = torch.mul(expanded_H, expanded_U) # (N, T, J, 4d)
+		#cat_data = torch.cat((expanded_H, expanded_U, HU), 3) # (N, T, J, 6d)
+		#S = self.similarity_layer(cat_data).view(-1, T, J) # (N, T, J, 1) => (N, T, J)
 
 		## compute ~U (context 2 query)
 		## softmax along J's dimension
 		## (N, T, 2d) = (N, T, J) X (N, J, 2d)
-		c2q = torch.bmm(F.softmax(S, dim=2), U)
 
+		#Query aware context representation.
+		c2q = torch.bmm(self.masked_softmax(S, U_mask), U)
+
+
+		#Context aware query representation
 		## compute ~h and expand to ~H
-		b = F.softmax(torch.max(S, 2)[0], -1)
+		masked_similarity = self.replace_masked_values(c2q,
+													   U_mask.unsqueeze(1),
+													   -1e7)
+		b = self.masked_softmax(torch.max(masked_similarity, dim=-1)[0].squeeze(-1), H_mask)
+
 		## (N, 1, 2d) = (N,1,T) * (N, T, 2d)
-		q2c = torch.bmm(b.unsqueeze(1), H)
+		q2c = torch.bmm(b.unsqueeze(1), H).squeeze(1)
+
+
 		## (N, 1, 2d) => (N, T, 2d)
-		tiled_q2c = q2c.repeat(1,T,1)
+		#tiled_q2c = q2c.repeat(1,T,1)
+		tiled_q2c = q2c.unsquueze(1).expand(batch_size, T, q2c.size(-1))
 
 		## G : concatenate H, ~U, H.~U and H.~H
 		# (N,T,2d): (N,T,2d) : (N,T,2d) => (N,T,6d)
-		G = torch.cat((H, c2q, torch.mul(H,c2q), torch.mul(H, tiled_q2c)), dim=2)
+		#G = torch.cat((H, c2q, torch.mul(H,c2q), torch.mul(H, tiled_q2c)), dim=2)
+
+		G = torch.cat([H,c2q,H * c2q,H*tiled_q2c],dim=-1)
+
 		return G, c2q, q2c
 

models/context_model.py
@@ -37,9 +37,9 @@ class ContextMRR(nn.Module):
 		self.output_layer = OutputLayer(output_layer_inputdim, hidden_size)
 
 
-	def forward(self, batch_query, batch_query_length,
-				batch_context, batch_context_length,
-				batch_candidates_sorted, batch_candidate_lengths_sorted, batch_candidate_unsort,
+	def forward(self, batch_query, batch_query_length,batch_query_mask,
+				batch_context, batch_context_length,batch_context_mask,
+				batch_candidates_sorted, batch_candidate_lengths_sorted, batch_candidate_masks_sorted,batch_candidate_unsort,
 				gold_index, negative_indices, batch_metrics):
 
 		## Embed query and context
@@ -56,7 +56,7 @@ class ContextMRR(nn.Module):
 
 		## BiDAF 1 to get ~U, ~h and G (8d) between context and query
 		# (N, T, 8d) , (N, T ,2d) , (N, 1, 2d)
-		context_attention_encoded, query_aware_context_encoded, context_aware_query_encoded = self.attention_flow_layer1(query_encoded, context_encoded)
+		context_attention_encoded, query_aware_context_encoded, context_aware_query_encoded = self.attention_flow_layer1(query_encoded, context_encoded,batch_query_mask,batch_context_mask)
 
 		## modelling layer 1
 		# (N, T, 8d) => (N, T, 2d)
@@ -70,7 +70,7 @@ class ContextMRR(nn.Module):
 		batch_candidates_embedded = self.word_embedding_layer(batch_candidates_sorted)
 		# (N1, K, 2d)
 		batch_candidates_encoded,_ = self.contextual_embedding_layer(batch_candidates_embedded, batch_candidate_lengths_sorted)
-		answer_attention_encoded, context_aware_answer_encoded, answer_aware_context_encoded = self.attention_flow_layer2(batch_context_modeled, batch_candidates_encoded)
+		answer_attention_encoded, context_aware_answer_encoded, answer_aware_context_encoded = self.attention_flow_layer2(batch_context_modeled, batch_candidates_encoded, batch_context_mask,batch_candidate_masks_sorted)
 
 		## modelling layer 2
 		# (N1, K, 8d) => (N1, K, 2d)
@@ -86,11 +86,12 @@ class ContextMRR(nn.Module):
 		gold_features = torch.index_select(answer_scores_unsorted, 0, index=gold_index)
 		negative_features = torch.index_select(answer_scores_unsorted, 0, index=negative_indices)
 
-		negative_metrics = torch.index_select(batch_metrics, 0, index=negative_indices)
-		negative_features = negative_features + negative_metrics.unsqueeze(1)
+		#negative_metrics = torch.index_select(batch_metrics, 0, index=negative_indices)
+		#negative_features = negative_features + negative_metrics.unsqueeze(1)
 		max_negative_feature, max_negative_index = torch.max(negative_features, 0)
 
-		loss = max_negative_feature - gold_features
+		#HingeLoss
+		loss = torch.clamp(1 - gold_features + max_negative_feature, 0)
 		return loss, max_negative_index
 
 