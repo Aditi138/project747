c441cd77dd52f3ae7e47d442f04805050af80338
Michiel29 <michiel.s.dejong@gmail.com>
2018-05-06 21:14:38 -0400

    keeping track of run files

--- score_run.sh
+++ score_run.sh
@@ -1,6 +1,7 @@
 python chunk_score.py \
 --train_path ../pickle/train \
 --valid_path ../pickle/valid \
+--pretrain_path ../embedding/glove100.txt \
 --max_documents 100 \
---embed_size 128 \
---hidden_size 128 \
+--embed_size 100 \
+--hidden_size 256 \
--- test_score.sh
+++ test_score.sh
@@ -1,3 +1,9 @@
 python -m pudb chunk_score.py \
 --train_path ../pickle/smalltrain \
 --valid_path ../pickle/smallvalid \
+--embed_size 100 \
+--hidden_size 128 \
+--max_documents 20 \
+--max_valid 20 \
+--eval_interval 5 \
+#--pretrain_path ../embedding/glove100.txt \
